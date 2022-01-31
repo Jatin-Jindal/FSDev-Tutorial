9bca138b085c8ce4cf9375599fa1456e0168ccac
GhostMander <58628835+GhostMander@users.noreply.github.com>
2022-01-31 21:08:50 +0530

    Added User

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -15,17 +20,37 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 Years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__, // only works in https
+      },
+      secret: "adaskjdhakhfukhakwhkahfkhwwaukwhjh",
+      resave: false,
+    })
+  );
+
   const appoloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
-      validate: false
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
     }),
-    context: () => ({ em: orm.em })
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
   appoloServer.applyMiddleware({ app });
 
-  
   app.listen(4000, () => {
     console.log("Server listening on port 4000");
   });
